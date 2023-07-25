f56753df1790f1dcf3ef7f3187cd3ee0484d8381
Sarayut Winuthayanon <s.winuthayanon@missouri.edu>
2023-07-25 13:52:31 -0500

    Updated

--- Nine.txt.sh
+++ Nine.txt.sh
@@ -2,6 +2,8 @@
 
 green sapling growing out of the ground, mud, dirt, grass, high quality, photorealistic, sharp focus, depth of field
 eagle, sun, roses, hyper-detailed, ultra-sharp, octane render, 8 k, illustration, highly detailed, simple, smooth and clean vector, no jagged lines, vector art, smooth
+A cute cat and dog are playing on the couch in front of the TV, high quality, photorealistic, sharp focus
+Ultra close-up photo of the face of fuzzy owl semi Panther with a rainbow Classic Rose color. Portrait photography, photorealism. a lot of details by Alex Shonenkov
 
 Negative prompt
 disfigured, kitsch, ugly, oversaturated, grain, low-res, Deformed, blurry, bad anatomy, disfigured, poorly drawn face, mutation, mutated, extra limb, ugly, poorly drawn hands, missing limb, blurry, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long neck, long body, ugly, disgusting, poorly drawn, childish, mutilated, mangled, old, surreal, people
