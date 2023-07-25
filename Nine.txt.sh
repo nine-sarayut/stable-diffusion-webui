6da26cfe2c1c92be89e5aeb637b2c819b81ce8f3
Sarayut Winuthayanon <s.winuthayanon@missouri.edu>
2023-07-25 13:40:04 -0500

    Updated

--- Nine.txt.sh
+++ Nine.txt.sh
@@ -2,3 +2,6 @@
 
 green sapling growing out of the ground, mud, dirt, grass, high quality, photorealistic, sharp focus, depth of field
 eagle, sun, roses, hyper-detailed, ultra-sharp, octane render, 8 k, illustration, highly detailed, simple, smooth and clean vector, no jagged lines, vector art, smooth
+
+Negative prompt
+disfigured, kitsch, ugly, oversaturated, grain, low-res, Deformed, blurry, bad anatomy, disfigured, poorly drawn face, mutation, mutated, extra limb, ugly, poorly drawn hands, missing limb, blurry, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long neck, long body, ugly, disgusting, poorly drawn, childish, mutilated, mangled, old, surreal, people
