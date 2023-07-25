344725f0bcfeba8edcd4ce7a1a0113ca590d74e4
Sarayut Winuthayanon <s.winuthayanon@missouri.edu>
2023-07-25 13:36:14 -0500

    Updated

--- Nine.txt.sh
+++ Nine.txt.sh
@@ -1,3 +1,10 @@
+git config --global user.name "Sarayut Winuthayanon"
+git config --global user.email "s.winuthayanon@missouri.edu"
+git config --global core.autocrlf input
+git config --global init.defaultBranch main
+git config --global core.editor "vim"
+
 ./webui.sh
 
 green sapling growing out of the ground, mud, dirt, grass, high quality, photorealistic, sharp focus, depth of field
+eagle, sun, roses, hyper-detailed, ultra-sharp, octane render, 8 k, illustration, highly detailed, simple, smooth and clean vector, no jagged lines, vector art, smooth
