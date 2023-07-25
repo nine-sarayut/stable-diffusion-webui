9e22488b1bf9bd2beebef4336a15261648930127
Sarayut Winuthayanon <s.winuthayanon@missouri.edu>
2023-07-25 15:02:27 -0500

    Updated

--- Nine.txt.sh
+++ Nine.txt.sh
@@ -1,10 +1,27 @@
 ./webui.sh
+civitai.com
 
 green sapling growing out of the ground, mud, dirt, grass, high quality, photorealistic, sharp focus, depth of field
 eagle, sun, roses, hyper-detailed, ultra-sharp, octane render, 8 k, illustration, highly detailed, simple, smooth and clean vector, no jagged lines, vector art, smooth
 A cute cat and dog are playing on the couch in front of the TV, high quality, photorealistic, sharp focus
 Ultra close-up photo of the face of fuzzy owl semi Panther with a rainbow Classic Rose color. Portrait photography, photorealism. a lot of details by Alex Shonenkov
 fuzzy owl semi Panther with a rainbow Classic Rose color. Portrait photography, photorealism
+
+8k portrait of beautiful cyborg with brown hair, intricate, elegant, highly detailed, majestic, digital photography, art by artgerm and ruan jia and greg rutkowski surreal painting gold butterfly filigree, broken glass, (masterpiece, sidelighting, finely detailed beautiful eyes: 1.2), hdr
+Negative prompt
+BadDream, FastNegativeV2
+
+https://civitai.com/models/4384/dreamshaper
+photo of the warrior Aragorn from Lord of the Rings, film grain
+BadDream, (UnrealisticDream:1.2)
+
+https://civitai.com/models/4201/realistic-vision-v50
+photo of the warrior Aragorn from Lord of the Rings, film grain
+BadDream, (UnrealisticDream:1.2)
+
+https://civitai.com/models/18798/meinaunreal?modelVersionId=112809
+realistic, 1girl, white hair, purple eyes, glowing eyes, crop top, skirt, parted lips, blush, night, flowers, sun, sunlight,
+(worst quality, low quality:1.4), monochrome, zombie, (interlocked fingers:1.2),
 
 Negative prompt
 disfigured, kitsch, ugly, oversaturated, grain, low-res, Deformed, blurry, bad anatomy, disfigured, poorly drawn face, mutation, mutated, extra limb, ugly, poorly drawn hands, missing limb, blurry, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long neck, long body, ugly, disgusting, poorly drawn, childish, mutilated, mangled, old, surreal, people
